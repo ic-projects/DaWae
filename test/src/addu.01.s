afb8e59b41312d23f3688b0e95a89707681ecc1f
Nikolay Smirnov <ns4516@ic.ac.uk>
2018-11-20 18:14:16 +0000

    Add tests for addu

---
+++ test/src/addu.01.s
@@ -0,0 +1,4 @@
+# Add two positive integers
+li $t1, 16
+li $t2, 8
+addu $v0, $t1, $t2
---
+++ test/src/addu.02.s
@@ -0,0 +1,4 @@
+# Add two large positive integers without overflow
+li $t1, 2147483000
+li $t2, 99999999
+addu $v0, $t1, $t2
