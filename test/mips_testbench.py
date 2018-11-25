8d77b573bfc2ef1afebaddd864974b5c7d059c93
Qiang Feng <Qiang.Feng@hotmail.co.uk>
2018-11-25 16:06:50 +0000

    Allow simulator executable to be specified in testbench
    Also print according to specification

--- test/mips_testbench.py
+++ test/mips_testbench.py
@@ -1,17 +1,18 @@
 from subprocess import Popen, PIPE
 import os
 import time
+import sys
 
 # Compile assembly test files into binary
-os.system('mkdir bin')
-for test in os.listdir('src'):
-    testName = test.strip('.s')
-    os.system('make test/bin/{}.mips.bin -C ../'.format(testName))
+os.system('mkdir -p test/bin')
+for test in os.listdir('test/src'):
+    testName = test[:-2]
+    os.system('make test/bin/{}.mips.bin > /dev/null'.format(testName))
 
 count = 0
 passCount = 0
-for test in os.listdir('bin'):
-    p = Popen(['../bin/mips_simulator', 'bin/' + test], stdout=PIPE, stderr=PIPE, stdin=PIPE)
+for test in os.listdir('test/bin'):
+    p = Popen([sys.argv[1], 'test/bin/' + test], stdout=PIPE, stderr=PIPE, stdin=PIPE)
     while p.poll() is None:
         time.sleep(0.1)
     exitCode = p.returncode
@@ -20,35 +21,22 @@ for test in os.listdir('bin'):
     # Remove .mips.bin file ending
     testName = test[:-9]
 
-    with open('./output/' + testName + '.mips.out', 'r') as f:
-        #Mod exit code by 256 since exit code size is only 8 bits
+    with open('test/output/' + testName + '.mips.out', 'r') as f:
+        # Exit code modulo 256 since exit code size is only 8 bits
         expectedExitCode = int(f.readline()) % 256
         expectedOut = f.readline()
 
-    # Get test description
-    descComment = open('./src/{}.s'.format(testName), 'r').readline()
-    comment = descComment.strip('# \n')
+    # Get test author and description
+    testFile = open('test/src/{}.s'.format(testName), 'r')
+    author = testFile.readline().strip('# \n')
+    description = testFile.readline().strip('# \n')
+    instruction = testName.split('.')[0].upper()
 
-    print('{}: {} - {} '.format(count, testName, comment))
-
-    exitCodePass = False
-    outputPass = False
-    # Checking exit code
-    if exitCode == expectedExitCode:
-        print('Exit Code - PASS')
-        exitCodePass = True
-    else:
-        print('Exit Code - FAIL (expected: {}, received: {})'.format(expectedExitCode, exitCode))
-
-    # Checking std::out
-    if output == expectedOut:
-        print('Output    - PASS')
-        outputPass = True
-    else:
-        print('Output - FAIL (expected: {}, received: {})'.format(expectedOut, output))
-
-    if exitCodePass and outputPass:
+    if exitCode == expectedExitCode and output == expectedOut:
+        print('{}, {}, PASS, {}'.format(testName.upper(), instruction, author))
         passCount += 1
+    else:
+        print('{}, {}, FAIL, {}, Exited with {} and expected {}; Output was "{}" and expected "{}"'.format(testName.upper(), instruction, author, exitCode, expectedExitCode, output, expectedOut))
 
     count += 1
 
