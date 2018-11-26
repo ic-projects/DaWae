11adb82b1e1687d36abd6a10b089f65a49ea538d
Nikolay Smirnov <ns4516@ic.ac.uk>
2018-11-26 14:37:56 +0000

    Improve divu tests

--- test/src/divu.07.s
+++ test/src/divu.07.s
@@ -1,9 +1,9 @@
 # ns4516
-# DIVU -112 by 12 checking quotient
+# DIVU -100 by 12 checking quotient
     .globl entry
 
 entry:
-    li $t0, -112
+    li $t0, -100
     li $t1, 12
     divu $t0, $t1
     mflo $v0
--- test/src/divu.08.s
+++ test/src/divu.08.s
@@ -1,9 +1,9 @@
 # ns4516
-# DIVU -112 by 12 checking remainder
+# DIVU -100 by 12 checking remainder
     .globl entry
 
 entry:
-    li $t0, -112
+    li $t0, -100
     li $t1, 12
     divu $t0, $t1
     mfhi $v0
