7ad83bbc3a222f94c9f067333a634840afe1b6e5
Nikolay Smirnov <ns4516@ic.ac.uk>
2018-11-26 17:06:30 +0000

    Fix bug in lb.10

--- test/src/lb.10.s
+++ test/src/lb.10.s
@@ -1,10 +1,10 @@
 # ns4516
-# LB from 0x20000000 with offset 0xFFFF initialised to be 0xFF
+# LB from 0x20000000 with offset 0x10000 initialised to be 0xFF
     .globl entry
 
 entry:
     li $t0, 0xFFFFFFFF
     li $t1, 0x20000000
-    sw $t0, 0xFFFF($t1)
-    lb $v0, 0xFFFF($t1)
+    sw $t0, 0x10000($t1)
+    lb $v0, 0x10000($t1)
     jr $zero
