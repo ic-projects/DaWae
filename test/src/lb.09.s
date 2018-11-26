18d29cd2c1ecd925ecf5495f500c52bfb105106c
Nikolay Smirnov <ns4516@ic.ac.uk>
2018-11-26 16:59:06 +0000

    Fix bug in lb.09

--- test/src/lb.09.s
+++ test/src/lb.09.s
@@ -5,6 +5,6 @@
 entry:
     li $t0, 0xFF
     li $t1, 0x20000000
-    sb $t0, 0($t0)
-    lb $v0, 0($t0)
+    sb $t0, 0($t1)
+    lb $v0, 0($t1)
     jr $zero
--- test/src/lb.10.s
+++ test/src/lb.10.s
@@ -5,6 +5,6 @@
 entry:
     li $t0, 0xFFFFFFFF
     li $t1, 0x20000000
-    sw $t0, 0($t0)
-    lb $v0, 0xFFFF($t0)
+    sw $t0, 0xFFFF($t1)
+    lb $v0, 0xFFFF($t1)
     jr $zero
